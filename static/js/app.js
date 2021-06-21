fbb3b0057ad1553b32ec7e83b86fd246d3790c24
Boyd Evans III <bbeiii@yahoo.com>
2021-06-21 09:20:57 -0600

    adding first variable for otu_ids

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,7 @@
 function makePlots(id) {
    d3.json("samples.json").then (sampledata =>{
        console.log(sampledata)
+       var otu_ids = sampledata.samples[0].otu_ids;
+       console.log(otu_ids)
    })
 }
