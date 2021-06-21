e18a5b1fba8871458d8c0d5dcc6ed8fc7a448660
Boyd Evans III <bbeiii@yahoo.com>
2021-06-21 09:37:59 -0600

    adding third variable for sample data labels

--- static/js/app.js
+++ static/js/app.js
@@ -5,6 +5,7 @@ function makePlots(id) {
        console.log(otuIds)
        var SampleValues = sampledata.samples[0].sample_values.slice(0,10).reverse();
        console.log(sampleValues)
-      
+       var otuLabels = sampledata.samples[0].otu_labels.slice(0,10);
+       console.log(otuLabels)
    })
 }
